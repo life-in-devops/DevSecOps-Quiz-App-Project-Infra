04e71c99327aa1071f6d90cbf933950b2a436362
life-in-devops <lifeindevops@gmail.com>
2025-02-14 16:17:22 +0530

    updated terraform script

--- terraform/iam-policy.tf
+++ terraform/iam-policy.tf
@@ -1,4 +1,4 @@
-resource "aws_iam_role_policy_attachment" "iam-policy" {
+resource "aws_iam_role_policy_attachment" "iam-bastion-policy" {
   role = aws_iam_role.iam-role.name
   # Just for testing purpose, don't try to give administrator access in production
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
